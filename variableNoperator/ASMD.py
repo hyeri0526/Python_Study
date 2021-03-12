da7a385622194e212f2060457f6acc0b5913eae9
hyeri0526 <80086193+hyeri0526@users.noreply.github.com>
2021-03-12 16:09:28 +0900

    Add files via upload

---
+++ variableNoperator/ASMD.py
@@ -0,0 +1,19 @@
+#정수를 입력받아 사친연산 결과를 출력
+'''a = int(input("첫 번째 숫자: "))
+b = int(input("두 번째 숫자: "))
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"*",b,"=",a*b)
+print(a,"/",b,"=",a/b)
+#블럭 주석 달기
+a = int(input("첫 번째 숫자: "))
+b = int(input("두 번째 숫자: "))
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"*",b,"=",a*b)
+print(a,"/",b,"=",a/b)'''
+
+x = int(input("첫 번째 숫자: "))
+y = int(input("두 번째 숫자: "))
+sum = x + y
+print(x,"+",y,"=",sum)
