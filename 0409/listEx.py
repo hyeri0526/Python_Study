9537c88456e1e34e6d88fb3e41cc1f808a53bc55
hyeri0526 <80086193+hyeri0526@users.noreply.github.com>
2021-04-09 15:52:42 +0900

    Add files via upload

---
+++ 0409/listEx.py
@@ -0,0 +1,10 @@
+fstName = ['L', 'e', 'e']
+sndName = ["Hye", "Ri"]
+stdNum = [2,0,2,1,0,6,0,2,8]
+
+print(fstName)
+print(sndName)
+#화면에 L, H, R을 출력하는 프로그램을 작성하시오.
+print(fstName[0]) # L출력
+print(sndName[0][0]) # H출력
+print(sndName[1][0]) # R출력
